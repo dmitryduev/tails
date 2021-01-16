13903eef79e204c81dcd7d8ac833b592bdb25aff
dmitryduev <dmitry.duev@gmail.com>
2021-01-15 18:12:48 -0800

    fix GA workflow

--- tests/test_run_tails.py
+++ tests/test_run_tails.py
@@ -1,11 +1,6 @@
 import pathlib
 
-from run_tails import run
-from tails.utils import load_config
-
-
-""" load config and secrets """
-config = load_config(config_file="config.yaml")["kowalski"]
+import run_tails
 
 
 class TestRunTails:
@@ -14,10 +9,10 @@ class TestRunTails:
     """
 
     def test_run_tails(self):
-        success = run(
+        success = run_tails.run(
             cleanup="all",
             checkpoint="../models/tails-20210107/tails",
-            config="../config.yaml",
+            config="../config.defaults.yaml",
             score_threshold=0.7,
             single_image="ztf_20191014495961_000570_zr_c05_o_q3",
         )
